4c638362ecc939b705174496735ea47962ab6158
Sarobidy <ratsaraefadahysarobidy@gmail.com>
2024-04-05 16:40:28 +0300

    Etat initial

---
+++ src/main/java/mg/itu/tp2prgsarobidy/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tp2prgsarobidy.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Configuration JSF
+ * @author Sarobidy
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
